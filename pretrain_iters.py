fffd216b50e591b83e4224c9fecd0cde658057eb
zguo0525 <zguo0525>
2022-10-23 17:41:24 -0400

    update

---
+++ pretrain_iters.py
@@ -0,0 +1,233 @@
+import json, argparse, datetime, os, time, math
+import numpy as np
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+import timm
+import timm.optim.optim_factory as optim_factory
+import util.misc as misc
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+import util.unet as models_unet
+from engine_pretrain import train_one_epoch
+import sys
+
+my_task_id = int(sys.argv[1])
+num_tasks = int(sys.argv[2])
+
+offset = 50
+
+starting_iters = [0, 5, 10, 15, 20, 25, 30]
+starting_iter = starting_iters[(my_task_id-1)//2]
+
+TV_weights = [0, 2]
+TV_weight = TV_weights[(my_task_id-1)%2]
+
+clip = True
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('model pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=32, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=400, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    parser.add_argument('--model', default='FBP_CNN_model0', type=str, metavar='MODEL',
+                        help='Name of model to train')
+
+    parser.add_argument('--input_size', default=128, type=int,
+                        help='images input size')
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=0.5 * 1e-3, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
+                        help='epochs to warmup LR')
+
+    # Dataset parameters
+    parser.add_argument('--output_dir', default='./weights/iters_model_0_offset_' + str(offset) + '_starting_iter_' + str(starting_iter) + '_weight_' + str(TV_weight),
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default=None,
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+class TomoDataset(Dataset):
+
+    def __init__(self, transform=None):
+        self.transform = transform
+        
+        data_tot = []
+        for i in range(10):
+            temp = np.load('./iterative_results/iterative_sparse_' + str(i) 
+                            + '_offset_' + str(offset) + '_weight_' + str(TV_weight) + '_clip_' + str(clip) + '.npy')[:1000, starting_iter//5, 0, 11:-11, 11:-11]
+            data_tot.append(temp)
+        data1 = (np.concatenate(data_tot,axis=0))
+        
+        data_tot = []
+        for i in range(10):
+            temp = np.load('full_angle_ic_1600_recons_' + str(i) + '_weight_0_clip_True.npy')[:1000, -1, 0, 11:-11, 11:-11]
+            data_tot.append(temp)
+        data2 = (np.concatenate(data_tot,axis=0))
+            
+        print(np.shape(data1))
+        print(np.shape(data2))
+        
+        self.data1 = np.expand_dims(data1, axis=-1)
+        self.data2 = np.expand_dims(data2, axis=-1)
+        self.data = np.concatenate((self.data1, self.data2), axis=-1)
+
+    def __len__(self):
+        return np.shape(self.data)[0]
+
+    def __getitem__(self, index):
+        image = self.data[index]
+        label = 0
+        if self.transform:
+            image = self.transform(image).float()
+        return (image, label)
+
+def main(args):
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = True
+
+    # simple augmentation
+    transform_train = transforms.Compose([
+            transforms.ToTensor()])
+    
+    dataset_train = TomoDataset(transform=transform_train)
+    print(dataset_train)
+
+    if True:  # args.distributed:
+        num_tasks = misc.get_world_size()
+        global_rank = misc.get_rank()
+        sampler_train = torch.utils.data.DistributedSampler(
+            dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
+        )
+        print("Sampler_train = %s" % str(sampler_train))
+    else:
+        sampler_train = torch.utils.data.RandomSampler(dataset_train)
+
+    if global_rank == 0 and args.log_dir is not None:
+        os.makedirs(args.log_dir, exist_ok=True)
+        log_writer = SummaryWriter(log_dir=args.log_dir)
+    else:
+        log_writer = None
+
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset_train, sampler=sampler_train,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=True,
+    )
+    
+    # define the model
+    model = models_unet.__dict__[args.model]()
+
+    model.to(device)
+
+    model_without_ddp = model
+    print("Model = %s" % str(model_without_ddp))
+
+    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+    
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
+    print("actual lr: %.2e" % args.lr)
+
+    print("accumulate grad iterations: %d" % args.accum_iter)
+    print("effective batch size: %d" % eff_batch_size)
+
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+        model_without_ddp = model.module
+    
+    # following timm: set wd as 0 for bias and norm layers
+    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    print(optimizer)
+    loss_scaler = NativeScaler()
+
+    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+        train_stats = train_one_epoch(
+            model, data_loader_train,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args
+        )
+        if args.output_dir and (epoch % 100 == 0 or epoch + 1 == args.epochs):
+            misc.save_model(
+                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                loss_scaler=loss_scaler, epoch=epoch)
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                        'epoch': epoch,}
+
+        if args.output_dir and misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+if __name__ == '__main__':
+    argv=sys.argv[4:]
+    args = get_args_parser()
+    args = args.parse_args(argv)
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
